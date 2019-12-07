5e900a695bd035c2c41c19b59cac6c89559a6c32
SophieNiu <27403989+SophieNiu@users.noreply.github.com>
2019-12-07 17:57:53 -0500

    save data to csv

--- final_proj.py
+++ final_proj.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import re
+import csv
+# import mysql.connector
+# import configparser
 # import plotly
 # import plotly.graph_objs as go
 
@@ -10,27 +14,25 @@ baseurl = "https://www.theinfatuation.com"
 
 
 class Bar:
-    def __init__(self, name, desc="", address=[""], phone_num='', url='', price='', hours=[]):
+    def __init__(self, name, rev="", addr="", phone_num='', url='', price='', hours=[], img='', neigh=''):
 
         self.name = name
-        self.address = address
-        self.desc = desc
+        self.addr = addr
+        self.review = rev
         self.phone = phone_num
         self.url = url
         self.price = price
         self.hours = hours
-
-        try:
-            self.description = desc
-            self.address = "{}, {},{} {}".format(
-                address_all[0], address_all[1], address_all[2], address_all[3]
-            )
-        except:
-            self.description = "No description found"
-            self.address = "No address found"
+        self.neigh = neigh
+        self.img = img
 
     def __str__(self):
-        return "{} is located at {}. Its phone number is {}. Its website is at {}".format(self.name, self.address, self.phone, self.url)
+        return "{} is located at {} in {}. Its phone number is {}. Its website is {}".format(self.name, self.addr, self.neigh, self.phone, self.url)
+
+    def set_neigh(self, neigh_dict):
+        if self.neigh in neigh_dict:
+            # update the neighborhood from the name to an ID: set up the foreign key
+            self.neigh = neigh_dict[self.neigh]
 
 
 def get_bars():
@@ -38,23 +40,17 @@ def get_bars():
     main_page = BeautifulSoup(
         make_request_using_cache(baseurl, main_url), 'html.parser')
     bars = main_page.findAll('div', class_="spot-block")
+    neighls = []
+    barls = []
 
-    for bar in bars[:10]:
-        bar_name = bar.find('h3').text.strip()
+    for bar in bars:
         # the page for next round of scripting, not the bar's url
         bar_rev_url = bar.find('a').get('href')
+
+        ### gather general bar info: name, price rating, address, neighborhood, img ###
+        bar_name = bar.find('h3').text.strip()
         bar_price = bar.find(
             'span', class_='address-price-rating')['data-price']
-        # the single line address
-        bar_addr = bar.find(
-            'div', class_='spot-block__address').text
-        for t in bar_addr:
-            bar_addr = bar_addr.replace('$', '').strip()
-
-        # find the neighborhood that the bar is located in
-
-        bar_neigh = bar.find('div', class_='overview-content')
-        print(bar_neigh)
 
         try:
             bar_img = bar.find('div', class_='spot-block__image-wrapper')
@@ -62,7 +58,29 @@ def get_bars():
         except Exception:
             bar_img = 'No image available.'
 
-        # scrape the individual page to get additional details
+        # the single line address
+        try:
+            bar_addr = bar.find(
+                'div', class_='spot-block__address').text
+            for t in bar_addr:
+                bar_addr = bar_addr.replace('$', '').strip()
+        except:
+            bar_addr = 'No address available.'
+        print(bar_name)
+
+        # find the neighborhood that the bar is located in
+        try:
+            bar_neigh = bar.find('div', class_='overview-content')
+            bar_neigh = bar_neigh.select(
+                'a[href^="/new-york/neighborhoods/"]')[0].text
+        except:
+            bar_neigh = 'No neighborhood identified.'
+
+        # create a list of neighborhoods
+        if bar_neigh not in neighls:
+            neighls.append(bar_neigh)
+
+        ### scrape the individual page to get additional details: phone, website, open hours, review###
         html = make_request_using_cache(baseurl, bar_rev_url)
 
         detail_page = BeautifulSoup(html, 'html.parser')
@@ -87,7 +105,47 @@ def get_bars():
             hours = open_day.find('p', class_='weekday__hours').text
             open_hours = (day, hours)
             opens.append(open_hours)
-        # print(opens)
+
+        # review contains double quotes and single quotes
+        bar_rev = detail_page.find(
+            'p' > 'div', class_='post__content__text-block').text
+
+        new_Bar = Bar(name=bar_name, rev=bar_rev, addr=bar_addr, phone_num=bar_phone,
+                      url=bar_url, price=bar_price, hours=opens, img=bar_img, neigh=bar_neigh)
+        barls.append(new_Bar)
+
+    return (barls, neighls)
+
+
+def create_bar_csv(barls):
+    with open('bars_info.csv', mode='w', newline='') as bars:
+        fieldnames = ['name', 'price', 'img', 'web',
+                      'addr', 'hours', 'review', 'neighborhood']
+        bar_writer = csv.DictWriter(bars, fieldnames=fieldnames)
+
+        bar_writer.writeheader()
+
+        for bar in barls:
+            bar_writer.writerow({'name': bar.name, 'price': bar.price, 'img': bar.img, 'web': bar.url,
+                                 'addr': bar.addr, 'hours': bar.hours, 'review': bar.review, 'neighborhood': bar.neigh})
+
+
+def create_neigh_csv(neigh_dict):
+    with open('neighborhoods.csv', mode='w', newline='') as bars:
+        fieldnames = ['id', 'name']
+        writer = csv.DictWriter(bars, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        for name, i in neigh_dict.items():
+            writer.writerow({'name': name, 'id': i})
+
+
+def get_neigh(neighls):
+    neigh_dict = {}
+    for i in range(len(neighls)):
+        neigh_dict[neighls[i]] = i+1
+    return neigh_dict
 
 
 def params_unique_combination(baseurl, endurl=""):
@@ -135,4 +193,10 @@ def make_request_using_cache(baseurl, endurl=""):
         return CACHE_DICTION[unique_ident]
 
 
-get_bars()
+(barls, neighls) = get_bars()
+neigh_dic = get_neigh(neighls)
+for bar in barls:
+    bar.set_neigh(neigh_dic)  # update the foreign key
+
+create_bar_csv(barls)
+create_neigh_csv(neigh_dic)
